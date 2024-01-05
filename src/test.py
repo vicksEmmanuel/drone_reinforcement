600ea61e03f6702e0c192045cbd79a17ab218743
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-01-05 09:42:19 +0000

    fix: testing path

--- src/test.py
+++ src/test.py
@@ -2,3 +2,6 @@ import os
 
 file_path = os.path.join(os.path.dirname(__file__), "../assets", "Drone.png")
 print("Loading image from:", file_path)
+
+file_path = os.path.join("src", "assets", "Bug.png")
+print("Loading image from:", file_path)
